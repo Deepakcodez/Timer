88243bb007c8301c90e869e984df9a88b480d593
Deepak <deepak2603dk@gmail.com>
2024-11-19 02:15:29 +0530

    project setup

---
+++ src/renderer/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  // const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+
+  return (
+    <>
+      <h1 className="bg-red-50">hello</h1>
+    </>
+  )
+}
+
+export default App
